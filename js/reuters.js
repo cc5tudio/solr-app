8a2ba1e2331356a2c76cbee820b787a290b83e39
Charles Campbell <charles@cc5tudio.com>
2017-10-21 22:35:57 -0400

    working with local solr instance

--- js/reuters.js
+++ js/reuters.js
@@ -25,12 +25,14 @@ define([
 ], function () {
   $(function () {
     Manager = new AjaxSolr.Manager({
-      solrUrl: 'http://reuters-demo.tree.ewdev.ca:9090/reuters/'
+      solrUrl: 'http://localhost:8983/solr/Documents/'
     });
+
     Manager.addWidget(new AjaxSolr.ResultWidget({
       id: 'result',
       target: '#docs'
     }));
+
     Manager.addWidget(new AjaxSolr.PagerWidget({
       id: 'pager',
       target: '#pager',
@@ -41,6 +43,7 @@ define([
         $('#pager-header').html($('<span></span>').text('displaying ' + Math.min(total, offset + 1) + ' to ' + Math.min(total, offset + perPage) + ' of ' + total));
       }
     }));
+
     var fields = [ 'topics', 'organisations', 'exchanges' ];
     for (var i = 0, l = fields.length; i < l; i++) {
       Manager.addWidget(new AjaxSolr.TagcloudWidget({
@@ -49,43 +52,50 @@ define([
         field: fields[i]
       }));
     }
+
     Manager.addWidget(new AjaxSolr.CurrentSearchWidget({
       id: 'currentsearch',
       target: '#selection'
     }));
+
     Manager.addWidget(new AjaxSolr.AutocompleteWidget({
       id: 'text',
       target: '#search',
-      fields: [ 'topics', 'organisations', 'exchanges' ]
+      fields: [ ]
     }));
+
     Manager.addWidget(new AjaxSolr.CountryCodeWidget({
       id: 'countries',
       target: '#countries',
       field: 'countryCodes'
     }));
+
     Manager.addWidget(new AjaxSolr.CalendarWidget({
       id: 'calendar',
       target: '#calendar',
       field: 'date'
     }));
+
     Manager.init();
     Manager.store.addByValue('q', '*:*');
     var params = {
       facet: true,
-      'facet.field': [ 'topics', 'organisations', 'exchanges', 'countryCodes' ],
+      //'facet.field': [ 'topics', 'organisations', 'exchanges', 'countryCodes' ],
       'facet.limit': 20,
       'facet.mincount': 1,
       'f.topics.facet.limit': 50,
       'f.countryCodes.facet.limit': -1,
       'facet.date': 'date',
-      'facet.date.start': '1987-02-26T00:00:00.000Z/DAY',
-      'facet.date.end': '1987-10-20T00:00:00.000Z/DAY+1DAY',
+      'facet.date.start': '2000-01-01T00:00:00.000Z/DAY',
+      'facet.date.end': '2017-01-01T00:00:00.000Z/DAY+1DAY',
       'facet.date.gap': '+1DAY',
       'json.nl': 'map'
     };
+
     for (var name in params) {
       Manager.store.addByValue(name, params[name]);
     }
+
     Manager.doRequest();
   });
 
